be4ea22817926d67b2e951192494e877395e9752
Faheelkhan1204 <153633614+Faheelkhan1204@users.noreply.github.com>
2024-03-24 03:51:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+/*Three logical operators
+&& , !! , ! */
+var armanBudget = 600000;
+var haveGirl = true;
+armanBudget >= 1000000 && haveGirl == true;
+console.log(armanBudget >= 1000000 && haveGirl == true);
+var haveDegree = "yes";
+var isFaheelsStd = "No";
+console.log(haveDegree == "yes");
+var num1 = 10;
+var result = !(num1 > 10);
+console.log(!(num1 > 10));
---
+++ app.ts
@@ -0,0 +1,15 @@
+/*Three logical operators 
+&& , !! , ! */ 
+let armanBudget=600000;
+let haveGirl=true;
+armanBudget >= 1000000 && haveGirl==true
+console.log(armanBudget>=1000000 && haveGirl==true)
+
+
+let haveDegree="yes";
+let isFaheelsStd="No";
+console.log(haveDegree=="yes" )
+
+let num1=10;
+let result=!(num1>10)
+console.log(!(num1>10))
